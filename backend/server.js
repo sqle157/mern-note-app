43e47f5373364b2977f71e90b88d953465a395a3
sqle157 <sql@Sangs-MacBook-Pro.local>
2022-11-07 14:34:37 -0700

    update package.json & prepare for deployment on render

--- backend/server.js
+++ backend/server.js
@@ -18,20 +18,6 @@ app.use((req, res, next) => {
 app.use('/api/users', require('./routes/users'));
 app.use('/api/notes', require('./routes/notes'));
 
-// Serve frontend
-if (process.env.NODE_ENV === 'production') {
-	// Set build folder as static
-	app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-	app.get('*', (req, res) =>
-		res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-	);
-} else {
-	app.get('/', (req, res) => {
-		res.status(200).json({ message: 'Welcome to the Note App API' });
-	});
-}
-
 // connect to db
 mongoose
 	.connect(process.env.MONGO_URI)
